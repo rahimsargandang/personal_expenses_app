95b5b030f6bb3c47ab51223023b1744e89d8880b
rahimsargandang <a178804@siswa.ukm.edu.my>
2022-05-20 01:25:13 +0800

    add styling

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:personal_expenses_app/transaction.dart';
+import './transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,15 +15,90 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'New item',
+      amount: 19.90,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't12',
+      title: 'New 12item',
+      amount: 29.90,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter App'),
       ),
-      body: Center(
-        child: Text('Widget Playground!'),
-      ),
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                child: Text('Chart', textAlign: TextAlign.center),
+                color: Colors.blue,
+                elevation: 5,
+              ),
+            ),
+            Column(
+              children: transactions.map((tx) {
+                return Card(
+                  color: Color.fromARGB(134, 204, 206, 93),
+                  child: Row(children: [
+                    Container(
+                      margin: EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 10,
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.red,
+                          width: 1,
+                        ),
+                      ),
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        tx.amount.toString(),
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.red,
+                        ),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text(
+                          tx.title,
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          tx.date.toString(),
+                          style: TextStyle(
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ]),
+                );
+              }).toList(),
+            ),
+          ]),
     );
   }
 }
